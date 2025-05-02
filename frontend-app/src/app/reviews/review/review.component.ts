4ea0d90c3550671d50be479f3051aa0827fe6cc2
Camille Berrier <camilleberrier@hotmail.com>
2025-05-02 17:14:08 +0200

    Fix review refresh

--- frontend-app/src/app/reviews/review/review.component.ts
+++ frontend-app/src/app/reviews/review/review.component.ts
@@ -4,23 +4,31 @@ import { AuthenticationService } from '../../login/authentication.service';
 import { Review } from '../../models/review.model';
 import { CommonModule } from '@angular/common';
 import { ReviewResponseComponent } from '../response/review-response/review-response.component';
-import { ReviewsComponent } from '../reviews.component';
-import { Subject } from 'rxjs';
+import { ProductService } from '../../products/product.service';
+import { AddReviewComponent } from '../add-review/add-review.component';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CommonModule, ReviewResponseComponent],
+  imports: [CommonModule, ReviewResponseComponent, AddReviewComponent],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
 export class ReviewComponent {
   @Input() reviewsByProduct!: Review[];
   respondingToReviewId: number | null = null;
+  allowedToComment: boolean = false;
+  isAddingReview: boolean = false;
   @Output() refreshRequest = new EventEmitter<boolean>();
+  
 
-  constructor(private reviewService: ReviewService, private authService: AuthenticationService){}
+  constructor(private reviewService: ReviewService, private authService: AuthenticationService, private productService: ProductService){}
+
+  ngOnInit(): void{
+    this.setAllowedToComment();
+ }
   getProductName(): string {
+    console.log(this.reviewsByProduct)
     return this.reviewsByProduct[0].product.name
   }
 
@@ -52,6 +60,41 @@ export class ReviewComponent {
   
   onResponseSubmitted(newResponse: Review): void {
     this.respondingToReviewId = null;
+    this.refreshRequest.emit(true);
+  }
+
+  isProductArtisan(){
+    return this.authService.getUserId() == this.reviewsByProduct[0].product.artisanId;
+  }
+
+  setAllowedToComment(){
+    const customerId = this.authService.getUserId(); // Ou autre méthode d'obtention du customerId
+
+    this.productService.GetReviewableProductsForCustomer(customerId).subscribe({
+      next: (reviewableProductIds: number[]) => {
+        this.allowedToComment = reviewableProductIds.includes(this.reviewsByProduct[0].productId);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des produits commentables', err);
+        this.allowedToComment = false;
+      }
+    });
+  }
+
+  canComment(): boolean {
+    return this.allowedToComment;
+  }
+
+  startAddingReview(): void {
+    this.isAddingReview = true;
+  }
+
+  cancelAddingReview(): void {
+    this.isAddingReview = false;
+  }
+
+  onReviewSubmitted(newReview: Review): void {
+    this.isAddingReview = false;
     this.refreshRequest.emit(true);
   }
 }
